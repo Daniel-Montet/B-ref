ce1bd28ecaaaa6175693ab97c43a022e0641afe2
Daniel Montet <dmontetproff@gmail.com>
2021-09-01 16:18:51 +0300

    addition of popup js file

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 let internalUrls = [],externalUrls = [];
 
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(async () => {
     chrome.storage.sync.set({ internalUrl: internalUrls, externalUrl: externalUrls });
     console.log(`Default state initialized:\nInternal Urls ---->${internalUrls}\nExternal Urls ---->${externalUrls}`);
 });
